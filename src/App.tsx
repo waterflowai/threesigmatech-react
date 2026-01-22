4574afb45a0426543098c2d000c7840d965c45d6
Oladipupoak <oladipupo.akinyan@kibo.school>
2026-01-22 15:57:33 +0100

    feat: initialize splash page project

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="splash-container">
+      <header className="header">
+        <span className="logo-text">ThreeSigmaTech</span>
+      </header>
+
+      <main className="content-row">
+        <div className="slogan-side">
+          <h1>Little by little we make the world better.</h1>
+        </div>
+
+        <div className="symbol-side">
+          <span className="big-symbol">3&Sigma;</span>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
